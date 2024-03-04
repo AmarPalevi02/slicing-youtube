0c33797a472f69d55ec1f644c7992a7cdb1f4831
amarNrddn <amarnrddn02@gmail.com>
2024-03-05 01:22:16 +0700

    update: searctbar input

--- src/components/orgabisems/searchbar/InputSearch.jsx
+++ src/components/orgabisems/searchbar/InputSearch.jsx
@@ -3,15 +3,15 @@ import { GoSearch } from "react-icons/go";
 
 const InputSearch = () => {
    return (
-      <div className='w-[22rem] h-8 relative'>
-         <div className="h-full">
+      <div className='w-[25rem] h-9 relative'>
+         <div className="h-full w-full flex items-center">
             <input
                type="text"
                placeholder='Telusuri'
-               className='bg-black border-[1px] rounded-xl w-full h-full pl-2'
+               className='bg-black border-[1px] border-neutral-800 rounded-tl-xl rounded-bl-xl w-full h-full pl-2 text-sm py-2 outline-none text-white'
             />
-            <button>
-               <GoSearch />
+            <button className=' bg-neutral-800 h-9 text-white w-12 rounded-tr-xl rounded-br-xl'>
+               <GoSearch className='w-full text-center font-bold' />
             </button>
          </div>
       </div>
